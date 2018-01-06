b686af5ff0f8fc44ad6af545891463dc822a3029
WIN-SN1HFN8BKRU\Chodae <hudsonchoi@gmail.com>
2018-01-06 16:57:31 -0500

    Template change

--- YJPackaging/Content/Scripts/main.js
+++ YJPackaging/Content/Scripts/main.js
@@ -1,38 +1,19 @@
 ﻿$(function () {
-    //alert(location.pathname.indexOf("Servcie"));
     if (location.pathname.indexOf("Service") >= 0) {
         $("#service").addClass("hilite");
-        $("#service a").css("color", "#ffffff");
-        $("#service a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("Technology") >= 0) {
         $("#technology").addClass("hilite");
-        $("#technology a").css("color", "#ffffff");
-        $("#technology a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("Clients") >= 0) {
         $("#clients").addClass("hilite");
-        $("#clients a").css("color", "#ffffff");
-        $("#clients a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("Company") >= 0) {
         $("#company").addClass("hilite");
-        $("#company a").css("color", "#ffffff");
-        $("#company a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("AboutUs") >= 0) {
         $("#aboutUs").addClass("hilite");
-        $("#aboutUs a").css("color", "#ffffff");
-        $("#aboutUs a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("ContactUs") >= 0) {
         $("#contactUs").addClass("hilite");
-        $("#contactUs a").css("color", "#ffffff");
-        $("#contactUs a").css("font-weight", "bold");
     } else if (location.pathname.indexOf("Referrals") >= 0) {
         $("#referrals").addClass("hilite");
-        $("#referrals a").css("color", "#ffffff");
-        $("#referrals a").css("font-weight", "bold");
     } else {
         $("#home").addClass("hilite");
-        $("#home a").css("color", "#ffffff");
-        $("#home a").css("font-weight", "bold");
     }
-
-
 }); 
